52290ed69924931a5bbadc3d5571abfffee112c8
ertuncertekin <ertekinertunc@gmail.com>
2024-05-10 17:46:57 +0300

    layout oluşturuldu ve css düzenlemeleri yapıldı

--- src/app/routes/home-page/home-page.component.ts
+++ src/app/routes/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BasicLayoutComponent } from '../../shared/components/basic-layout/basic-layout.component';
 
 @Component({
   selector: 'home-page',
   standalone: true,
   imports: [
-    CommonModule, RouterModule
+    CommonModule, RouterModule, BasicLayoutComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
