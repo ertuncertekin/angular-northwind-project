023776183c0198430751b0fab6afb987ac30d341
ertuncertekin <ertekinertunc@gmail.com>
2024-05-10 15:39:29 +0300

    for döngüsü farklı kullanımları

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,4 +29,25 @@ export class AppComponent {
   getFiltiredItems() {
     return this.items.filter(item => item > 1)
   }
+
+  products: { name: string; price: number; discontinued: boolean }[] = [
+    { name: "Kalem", price: 100, discontinued: false },
+    { name: "Defter", price: 80, discontinued: false },
+    { name: "Kurşun Kalem", price: 50, discontinued: true },
+    { name: "Silgi", price: 30, discontinued: false },
+    { name: "Kalemtıraş", price: 20, discontinued: true },
+    { name: "Rulo Cetvel", price: 70, discontinued: false },
+    { name: "Dosya", price: 45, discontinued: false },
+    { name: "Mürekkep", price: 60, discontinued: true }
+  ];
+
+
+  get FilteredProducts() {
+    return this.products.filter(product => product.price > 50)
+  }
+
+  // getFilteredProducts() {
+  //   return this.products.filter(product => product.price > 50).map(filteredProduct => filteredProduct.name);
+  // }
+
 }
