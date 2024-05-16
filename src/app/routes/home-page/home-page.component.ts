1e977bd5c491a875b2ad1835f9010e0b4d86bdbc
ertuncertekin <ertekinertunc@gmail.com>
2024-05-16 16:52:27 +0300

    h1 elementi eklendi

--- src/app/routes/home-page/home-page.component.ts
+++ src/app/routes/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { BasicLayoutComponent } from '../../shared/components/basic-layout/basic-layout.component';
 import { CategoryListGoupComponent } from '../../features/categories/components/category-list-goup/category-list-goup.component';
 import { ProductCardListComponent } from '../../features/products/components/product-card-list/product-card-list.component';
 import { CategoryListItem } from '../../features/categories/models/category-list-item';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'home-page',
@@ -14,7 +16,7 @@ import { CategoryListItem } from '../../features/categories/models/category-list
     RouterModule,
     BasicLayoutComponent,
     CategoryListGoupComponent,
-    ProductCardListComponent
+    ProductCardListComponent,
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
@@ -22,6 +24,14 @@ import { CategoryListItem } from '../../features/categories/models/category-list
 })
 export class HomePageComponent {
   seletectedCategory: CategoryListItem | null = null;
+
+  /*constructor(private router: Router, route: ActivatedRoute) {
+    if (this.seletectedCategory) {
+      this.router.navigate([], {
+        queryParams: { category: this.seletectedCategory.id }
+      })
+    }
+  }*/
 
   onChangeSelectCategory(event: { selectedCategory: CategoryListItem | null }) {
     this.seletectedCategory = event.selectedCategory;
